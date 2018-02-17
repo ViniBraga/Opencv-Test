112afc3c8e59deebb922bf90ba7143d934245d9f
ViniBraga <vinicius.braga99@gmail.com>
2018-02-17 17:50:57 -0200

    first commit

---
+++ Opencv-Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Opencv-Test
+//
+//  Created by Vinicius Braga on 21/08/16.
+//  Copyright © 2016 Vinicius Braga. All rights reserved.
+//
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+    return 0;
+
+}
